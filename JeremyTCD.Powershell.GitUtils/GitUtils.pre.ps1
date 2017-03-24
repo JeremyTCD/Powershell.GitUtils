c83ceed31e8ad0733803f772a2dd549d8542a88d
JeremyTCD <jeremytanchongdao@gmail.com>
2017-03-24 21:22:14 +0800

    GitUtils.pre.ps1 now throws error if git is not available

--- JeremyTCD.Powershell.GitUtils/GitUtils.pre.ps1
+++ JeremyTCD.Powershell.GitUtils/GitUtils.pre.ps1
@@ -1,4 +1,4 @@
 if ((Get-Command "git" -ErrorAction SilentlyContinue) -eq $null) 
 { 
-   Write-Host "Error: please install git and add it to your path"
+   throw "Error: Git not available. Please install git and add it to your path."
 }
