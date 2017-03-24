8ceecc3c61edb9cd5def0a14e971cb44d3b3a779
JeremyTCD <jeremytanchongdao@gmail.com>
2017-03-24 22:49:54 +0800

    Added Set-ChangelogFromTags

--- JeremyTCD.Powershell.GitUtils/GitUtils.psm1
+++ JeremyTCD.Powershell.GitUtils/GitUtils.psm1
@@ -1,11 +1,18 @@
 function Read-GitTagMessage{
 	param([string] $tag)
 
-	$message = (git tag -l --format '%(contents)' $tag)[0]
+	$message = git tag -l --format '%(contents)' $tag
+
 	return $message
 }
 
 function Read-AllGitTagMessages{
-	$messages = (git tag -l --format '%(contents)')[0]
+	$messages = git tag -l --format '%(contents)'
+
 	return $messages
+}
+
+function Set-ChangelogFromTags{
+    '# Changelog' | Out-File 'Changelog.md'
+	Read-AllGitTagMessages | Out-File 'Changelog.md' -Append 
 }
