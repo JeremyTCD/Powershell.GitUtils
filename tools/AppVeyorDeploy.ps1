40ea3199f84ccef72f4cd06a58731b40c55f97fe
JeremyTCD <jeremytanchongdao@gmail.com>
2017-03-27 22:38:20 +0800

    Updated build script to use Push-ChangelogFromTags

--- tools/AppVeyorDeploy.ps1
+++ tools/AppVeyorDeploy.ps1
@@ -1,25 +1,13 @@
-cd C:\projects\powershell-gitutils
 git config --global credential.helper store
 Add-Content "$($env:USERPROFILE)\.git-credentials" "https://$($env:git_key):x-oauth-basic@github.com`n"
+
 git config --global user.email $env:build_user_email
 git config --global user.name $env:build_user
-git checkout master -q
-Remove-Module GitUtils
-Import-Module '.\src\GitUtils\GitUtils.psd1'
-Set-ChangelogFromTags
-git add --intent-to-add .
-git diff --quiet
 
-if($lastexitcode -eq 1){
-    git add .
-    git commit -m 'Updated changelog' -q
-    git push origin master -q
-    if($lastexitcode -ne 0){
-        Write-Error 'Changelog update failed'
-    }
-}
+Push-ChangelogFromTags
    
 $latestVersion = git describe --abbrev=0
+# The following should be moved into a Publish-Powershell function
 $moduleExists = Find-Module -Name GitUtils -RequiredVersion $latestVersion
 if(!$moduleExists){
     Update-ModuleManifest -Path './src/GitUtils/GitUtils.psd1' -ModuleVersion $latestVersion
