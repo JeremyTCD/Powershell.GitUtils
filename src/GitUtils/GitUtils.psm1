7ded4928a7e0db583b11c0439b8e98febf953b77
JeremyTCD <jeremytanchongdao@gmail.com>
2017-03-27 22:34:04 +0800

    Added private function Invoke-Git
    - For mocking

--- src/GitUtils/GitUtils.psm1
+++ src/GitUtils/GitUtils.psm1
@@ -1,10 +1,15 @@
+<# Abstraction layer for mocking #>
+function Invoke-Git{
+	git $args
+}
+
 function Read-TagMessage{
 	[CmdletBinding()]
 	[OutputType([String])]
 	param([Parameter(Mandatory=$true)][string] $tag)
 
-	$message = git tag -l --format '%(contents)' $tag
-	# git tag -l returns multiline contents as an array
+	$message = Invoke-Git tag -l --format '%(contents)' $tag
+	# Invoke-Git tag -l returns multiline contents as an array
 	# with an empty string item between each tag
 	$message = $message[0..($message.Length - 2)]
 	$message = $message -join "`n"
@@ -16,7 +21,7 @@ function Read-AllTagMessages{
 	[CmdletBinding()]
 	[OutputType([String])]
 	
-	$messages = git tag --sort=-v:refname --format '%(contents)'
+	$messages = Invoke-Git tag --sort=-v:refname --format '%(contents)'
 	$messages = $messages -join "`n"
 
 	return $messages
